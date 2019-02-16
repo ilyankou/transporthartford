8e226d52fdd1aa582f3324cda1697bb69d2235a2
ilyankou <ilyankou@gmail.com>
2019-02-16 13:05:55 -0500

    Add scatterplot

--- fatalities/2_tidy.py
+++ fatalities/2_tidy.py
@@ -5,5 +5,7 @@ fatalities = pd.read_csv('data/fatalities-deduped.csv', dtype=str)
 fatalities['DUI'] = fatalities['Condition at Time of Crash'].apply(lambda x: True if x == 'Under the Influence of Medications/Drugs/Alcohol' else False)
 fatalities['Speeding'] = fatalities['Speeding Related'].apply(lambda x: True if x in ['Too Fast for Conditions', 'Exceeded Speed Limit', 'Racing'] else False)
 
-fatalities.groupby(['Town', 'Year', 'Age', 'Gender', 'DUI', 'Speeding']).size().to_frame('Count').to_csv('data/tidy-by-town-by-year-by-age.csv', header=True)
-fatalities.groupby(['Age', 'Gender', 'Person Type', 'DUI', 'Speeding']).size().to_frame('Count').to_csv('data/tidy-by-age.csv')
+fatalities[(fatalities.Year >= '2013') & (fatalities.Year <= '2017')].groupby(['Town']).size().to_frame('AvFatalities').to_csv('data/av-fatalities-13-17.csv')
+
+#fatalities.groupby(['Town', 'Year', 'Age', 'Gender', 'DUI', 'Speeding']).size().to_frame('Count').to_csv('data/tidy-by-town-by-year-by-age.csv', header=True)
+#fatalities.groupby(['Age', 'Gender', 'Person Type', 'DUI', 'Speeding']).size().to_frame('Count').to_csv('data/tidy-by-age.csv')
